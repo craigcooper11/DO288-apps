4be25e9a0b62514eb17b49477603fc1fb6169729
Craig Cooper <craigcooper87911@gmail.com>
2024-01-02 16:52:26 -0600

    test

--- php-hello-dockerfile/index.php
+++ php-hello-dockerfile/index.php
@@ -1,16 +1,22 @@
 <?php
-echo "Hello, World! PHP version is!" . PHP_VERSION . "\n";
-$servername = getenv("HOST");
-$username = getenv("USER");
-$password = getenv("PASS");
+echo "Hello, World! PHP version is " . PHP_VERSION . " :)\n";
+try {
+    $servername = getenv("HOST");
+    $username = getenv("USER");
+    $password = getenv("PASS");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-    echo("Connection failed: " . $conn->connect_error);
-    die();
+    // Check connection
+    if ($conn->connect_error) {
+        echo("Connection failed: " . $conn->connect_error);
+        die();
+    }
+}
+catch(\Throwable $th) {
+    echo "HERE!";
+    print_r($th);
 }
 echo("Connected successfully");
 ?>
