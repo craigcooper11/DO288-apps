b016b931d3125dc06e9871a333153ef7b867857a
Craig Cooper <craigcooper87911@gmail.com>
2024-01-02 17:26:06 -0600

    test

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -1,5 +1,17 @@
 var express = require('express');
 app = express();
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
