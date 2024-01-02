3346f986e330049d5031d74c21e406ca5e4f09fc
Craig Cooper <craigcooper87911@gmail.com>
2024-01-02 17:40:28 -0600

    test

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -5,13 +5,22 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
-  password: process.env.PASS
+  password: process.env.PASS,
+  database: "sampledb"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  var test = "Hi" + new Date().getTime()
+  var sql = `INSERT INTO customers (name, address) VALUES ('${test}', 'Highway 37')`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
 });
+
+con.query
 
 app.get('/', function (req, res) {
   res.send(con.state)
